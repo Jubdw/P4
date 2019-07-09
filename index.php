7903dbb1063fd9c9bbf1b2a85e3b6d218b9b2066
Julien Barré <julienbarre01@ymail.com>
2019-07-09 22:08:00 +0200

    maj routeur index.php

--- index.php
+++ index.php
@@ -50,6 +50,23 @@ try {
                 throw new Exception('Aucun identifiant de commentaire envoyé');
             }
         }
+        elseif ($_GET['action'] == 'registerUser') {
+            if (!empty($_POST['password']) && !empty($_POST['password_check']) && $_POST['password'] == $_POST['password_check']) {
+                if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email'])) {
+                    $hashed_pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                    addUser($_POST['name'], $hashed_pass, $_POST['email']);
+                }
+                else {
+                    throw new Exception('L\'adresse email n\'est pas valide !');
+                }
+            }
+            else {
+                throw new Exception('Le mot de passe est différent d\'un champ à l\'autre');
+            }
+        }
+        elseif ($_GET['action'] == 'register') {
+            register();
+        }
         elseif ($_GET['action'] == 'about') {
             about();
         }
