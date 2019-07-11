64a28ce2cab240fed8ae767f07bf8e851103ec58
Julien Barré <julienbarre01@ymail.com>
2019-07-11 17:56:52 +0200

    Maj profileView et style.css - bouton modifier commentaire

--- view/frontend/profileView.php
+++ view/frontend/profileView.php
@@ -34,6 +34,25 @@ if (isset($_SESSION['id']) AND isset($_SESSION['name'])) {
 	<div class="comment">
 		<em><strong>Le <?= $data['comment_date_fr'] ?></strong></em>
 		<p><?= nl2br(htmlspecialchars($data['comment'])) ?></p>
+		<?php 
+		if (isset($_SESSION['id']) AND isset($_SESSION['name'])) {
+			if ($_SESSION['name'] == $_GET['name']) 
+			{
+			?>
+			<div class="link-update"><a href="index.php?action=update&amp;id=<?= $data['id'] ?>">Modifier</a></div>
+			<?php
+			}
+			else 
+			{
+			?>
+			<a href="#">Signaler</a>
+			<?php
+			}
+		}
+		?>
+		<br>
+		<div class="line-comment"></div>
+		<br>	
 	</div>
 	<?php
 	}
