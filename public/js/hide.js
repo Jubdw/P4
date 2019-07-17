523991770a377d94301e0af9998fb765360fb42a
Julien Barré <julienbarre01@ymail.com>
2019-07-17 17:54:36 +0200

    Effacement du fichier hide.js (inutile)

--- public/js/hide.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('hide-button').addEventListener('click', () => document.getElementById('user-management').style.height = "50px");
-}
