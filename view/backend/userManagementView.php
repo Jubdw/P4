bfc60735ef35d447a84d65f05541c7296b42fda0
Julien Barré <julienbarre01@ymail.com>
2019-07-22 19:55:57 +0200

    Maj userManagementView - utilisateurs paginés - 5 par page

--- view/backend/userManagementView.php
+++ view/backend/userManagementView.php
@@ -47,6 +47,24 @@
 	<?php 
 	}
 	?>
+	<div class="paging">
+		<?php 
+		for ($i = 1; $i <= $maxPages; $i++) {
+			if ($i == $currentPage) 
+			{
+			?>
+			<div class="current-page"><p> <?= $i ?> </p></div>
+			<?php 
+			}
+			else 
+			{
+			?>
+			<div class="other-pages"><a href="index.php?action=userManagement&amp;page=<?= $i ?>"> <?= $i ?> </a></div>
+			<?php 
+			}
+		}
+		?>
+	</div>
 </div>
 
 <?php $content = ob_get_clean(); ?>
