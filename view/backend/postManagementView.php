38ceab9f5613b9141138c535a748001fa4df7a5d
Julien Barré <julienbarre01@ymail.com>
2019-07-25 23:10:32 +0200

    Maj postManagementView - présentation générale

--- view/backend/postManagementView.php
+++ view/backend/postManagementView.php
@@ -17,18 +17,18 @@
 	<div class="post-admin-list">
 		<div class="small-post">
 			<h4><?= $posts['title'] ?></h4>
-			<em>Le : <?= $posts['creation_date_fr'] ?></em>
+			<strong><em>Le : <?= $posts['creation_date_fr'] ?></em></strong>
 			<p><?= $posts['small_content'] ?>...</p>
 		</div>
 		<div class=" post-manage">
 			<div class="post-link">
-				<a href="index.php?action=post&amp;id=<?= $posts['id'] ?>&amp;page=1">Voir le chapitre</a>
+				<a href="index.php?action=post&amp;id=<?= $posts['id'] ?>&amp;page=1">&#128270;<br>Lire<br>&#128270;<br></a>
 			</div>
 			<div class="post-edit">
-				<a href="index.php?action=postToEdit&amp;id=<?= $posts['id'] ?>">Modifier</a>
+				<a href="index.php?action=postToEdit&amp;id=<?= $posts['id'] ?>">&#10004;<br>Modifier<br>&#10004;</a>
 			</div>
 			<div class="post-delete">
-				<a href="index.php?action=deletePost&amp;id=<?= $posts['id'] ?>" onclick="return(confirm('Cette action est définitive. Etes-vous sûr de vouloir supprimer ce chapitre ?'));">Effacer</a>
+				<a href="index.php?action=deletePost&amp;id=<?= $posts['id'] ?>" onclick="return(confirm('Cette action est définitive. Etes-vous sûr de vouloir supprimer ce chapitre ?'));">&#128683;<br>Effacer<br>&#128683;</a>
 			</div>
 		</div>
 	</div>
