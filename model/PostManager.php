21cf57f6c392a50fbcfaeb8c97a7afd52bb3d0b0
Julien Barré <julienbarre01@ymail.com>
2019-07-26 01:55:37 +0200

    Maj Model PostManager - pagination des post sur listPostsView

--- model/PostManager.php
+++ model/PostManager.php
@@ -6,10 +6,23 @@ require_once("model/Manager.php");
 
 class PostManager extends Manager
 {
-    public function getPosts()
+    public function countPosts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, SUBSTRING(content, 1, 255) AS small_content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT COUNT(id) as postsNb FROM posts');
+        $data = $req->fetch();
+
+        return $data;
+    }
+
+    public function getPostsPaged($start, $perPage)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, SUBSTRING(content, 1, 255) AS small_content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT :start, :perPage');
+
+        $req->bindValue('start', $start, \PDO::PARAM_INT);
+        $req->bindValue('perPage', $perPage, \PDO::PARAM_INT);
+        $req->execute();
 
         return $req;
     }
